214ca7ea6ce5e0ae4ab0b8ee10c11de36bba8333
khuongdoanngoc <khuong.dngc@gmail.com>
2025-03-12 22:35:41 +0700

    feat(SASSSJ-16): add micro speech to ai chatbot page

---
+++ src/hooks/useSpeechToText.ts
@@ -0,0 +1,38 @@
+import { useEffect, useRef, useState } from "react";
+
+export const useSpeechToText = () => {
+    const [isListening, setIsListening] = useState(false);
+    const [transcript, setTranscript] = useState("");
+    const recgonitionRef = useRef<any>(null);
+
+    useEffect(() => {
+        if (!("webkitSpeechRecognition" in window)) {
+            alert(
+                "Your browser does not support the Web Speech API. Please try Google Chrome or Edge."
+            );
+            return;
+        }
+        const recognition = new (window as any).webkitSpeechRecognition();
+        recognition.continuous = true;
+        recognition.interimResults = true;
+        recognition.lang = "vi-VN";
+        recgonitionRef.current = recognition;
+        recgonitionRef.current.onresult = (event: any) => {
+            const transcript = event.results[0][0].transcript;
+            setTranscript(transcript);
+        };
+    }, []);
+
+    const startListening = () => {
+        setTranscript("");
+        setIsListening(true);
+        recgonitionRef.current.start();
+    };
+
+    const stopListening = () => {
+        setIsListening(false);
+        recgonitionRef.current.stop();
+    };
+
+    return { transcript, isListening, startListening, stopListening };
+};
